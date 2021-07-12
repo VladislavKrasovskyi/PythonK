4a0e5866ca45fea61182e6db32ef439729acd8f9
Vladislav Krasovskyi <desant.voora@gmail.com>
2021-07-12 14:45:31 +0300

    Update task1.py

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,46 +1,38 @@
-import re
-def find_str(find_str, method):
-    if method == 0:
-        integer_stroka = re.findall(r'\d+', find_str)
-        integer_stroka = [int(i) for i in integer_stroka]
-        return integer_stroka
-    elif method == 1:
-        string_stroka = re.findall(r'\D', find_str)
-        string_stroka = [str(i) for i in string_stroka]
-        string_stroka = ''.join(string_stroka)
-        return string_stroka
-    else:
-        return "Виникла помилка!"
+line = input("Введіть рядок для опрацювання: ")
+lineSplit = line.split()
+numbers = []
 
-def Degree(integer_stroka):
-    maxi = max(integer_stroka)
-    res = []
-    for i in range(len(integer_stroka)):
-        if integer_stroka[i] != maxi:
-            res.append(integer_stroka[i]**(i+1))
-        else:
-            res.append(integer_stroka[i])
-    return res
+i = 0
+while i < len(lineSplit):
+    if lineSplit[i].isdecimal():
+        numbers.append(int(lineSplit.pop(i)))
+        continue
+    digits = "".join(filter(lambda i: i.isdecimal(), lineSplit[i]))
+    if digits:
+        numbers.append(int(digits))
+        lineSplit[i] = "".join(filter(lambda i: not i.isdecimal(), lineSplit[i]))
+    i += 1
 
-def BigLetters(string_stroka):
-    res = ""
-    k = 0
-    spl = str(string_stroka).split()
-    for i in spl:
-        for i in spl[k].split():
-            a = ''.join(spl[k])
-            if len(a) == 1:
-                res += a[0].upper()+' '
-            else:
-                res += a[0].upper()+a[1:-1]+a[-1].upper() + ' '
-            k += 1
-    return res
+lineSplit = list(map(lambda item: item.title()[:-1] + item[-1].upper(), lineSplit))
 
-stroka = input("Введіть вашу стрічку з букв та цифр:\n")
-print("Ваша стрічка:", stroka)
-integer_stroka = find_str(stroka, 0)
-print("Стрічка з цифрами:\n", integer_stroka)
-string_stroka = find_str(stroka, 1)
-print("Стрічка з буквами:\n", string_stroka)
-print("Стрічка в якій перша та останні букви великі:\n", BigLetters(string_stroka))
-print("Стрічка в якій числа піднесені до степеня їх індекса:\n",Degree(integer_stroka))
+maxNumber = 0
+numbersPowered = list()
+if numbers:
+    maxNumber = max(numbers)
+    numbersPowered = list(
+        map(lambda x: x ** numbers.index(x) if x != maxNumber else x, numbers)
+    )
+
+lineEdited = " ".join(lineSplit)
+
+print("", f"Ваша стрічка: {line}", "", f"Стрічка після редагування: {lineEdited}", "", sep="\n")
+
+if numbers:
+    print(
+        f"Числа: {numbers}",
+        "",
+        f"Максимальне число: {maxNumber}",
+        "",
+        f"Числа після долучення: {numbersPowered}",
+        sep="\n",
+    )
